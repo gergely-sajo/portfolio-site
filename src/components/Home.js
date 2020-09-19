21ce5343ff261022393d89520af7934bff85315f
gergely-sajo <sajo.gergo@gmail.com>
2020-09-19 21:47:00 +0200

    Fixed the issue of 100vh

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,23 @@ import React from 'react';
 import me from '../images/me.jpeg';
 
 const Home = ({ element }) => {
+
+  let winHeight = window.innerHeight
+
   return (
-    <div id="home" className="page-section page-section--dark page-section-full-vh">
-        <div className="wrapper home-banner__content">
-            <div id="Home-Banner-Content" className="home-banner__description">
-                <h1 ref={element} className="home-banner__title">Hey, I'm Gary.</h1>
-                <p className="home-banner__subtitle">Self-taught developer from Vienna, Austria. Currently, I am looking for an opportunity to begin my career in web development.</p>
-                <a href="#my-projects"><button className="btn btn--large">My Projects</button></a>
-            </div>
-            <div id="Home-Banner-Photo" className="home-banner__photo">
-                <img src={me} alt="Gary Sajo" alt="That´s me"/>
-            </div>
+    <div id="home" className="page-section page-section--dark" style={{height: winHeight}}>
+      <div className="wrapper home-banner__content">
+        <div id="Home-Banner-Content" className="home-banner__description">
+          <h1 ref={element} className="home-banner__title">Hey, I'm Gary.</h1>
+          <p className="home-banner__subtitle">Self-taught developer from Vienna, Austria. Currently, I am looking for an opportunity to begin my career in web development.</p>
+          <a href="#my-projects"><button className="btn btn--large">My Projects</button></a>
         </div>
+        <div id="Home-Banner-Photo" className="home-banner__photo">
+          <img src={me} alt="Gary Sajo" alt="That´s me"/>
+        </div>
+      </div>
     </div>
-    )
+  )
 }
 
 export default Home;
